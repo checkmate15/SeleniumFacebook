aa12bb96edd339bafcb37256b8c1c4fb517bff09
checkmate15 <ahmed.adnan57@outlook.com>
2019-05-01 19:00:29 -0400

    New update with firefox browser and 4 more test

--- src/main/java/initialization/Setup.java
+++ src/main/java/initialization/Setup.java
@@ -2,10 +2,15 @@ package initialization;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 public class Setup {
@@ -15,16 +20,35 @@ public class Setup {
     public static WebDriver SetupDriver(String platform, String url, String browser) {
 
         //connection to platform and blows
-        if (platform.equalsIgnoreCase("mac") && browser.equalsIgnoreCase("Chrome")) {
+        if (platform.equalsIgnoreCase("mac")&&(browser.equalsIgnoreCase("Chrome"))){
+
             //if platform is mac
             System.getProperty("webdriver.chrome.driver", "C:\\Users\\Adnan Ahmed\\IdeaProjects\\Google\\src\\main\\resources\\drivers\\chromedrivernew");
-        } else {
-            //if the platform is Windows
-            System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adnan Ahmed\\IdeaProjects\\Google\\src\\main\\resources\\drivers\\chromedrivernew.exe");
+            driver = new ChromeDriver();
+        }else if (platform.equalsIgnoreCase("mac")&&(browser.equalsIgnoreCase("Firefox"))) {
+                System.getProperty("webdriver.gecko.driver", "C:\\Users\\Adnan Ahmed\\IdeaProjects\\Google\\src\\main\\resources\\drivers\\geckodriver");
+
+                driver = new FirefoxDriver();
+
+        } else if(platform.equalsIgnoreCase("windows")&&(browser.equalsIgnoreCase("chrome"))){
+                //if the platform is Windows
+                System.setProperty("webdriver.chrome.driver", "C:\\Users\\Adnan Ahmed\\IdeaProjects\\Google\\src\\main\\resources\\drivers\\chromedrivernew.exe");
+
+                driver = new ChromeDriver();
+
+        }else if(platform.equalsIgnoreCase("windows")&&(browser.equalsIgnoreCase("Firefox"))){
+
+                System.setProperty("webdriver.gecko.driver", "C:\\Users\\Adnan Ahmed\\IdeaProjects\\Google\\src\\main\\resources\\drivers\\geckodriver.exe");
+
+                driver = new FirefoxDriver();
 
         }
+
+
+
+
         //connect to WebDriver interface and create object to ChromeDriver class
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
@@ -40,7 +64,7 @@ public class Setup {
         return driver;
     }
 
-    @AfterTest
+    @AfterMethod
     public void quitDriver() {
         driver.quit();
     }
@@ -48,7 +72,7 @@ public class Setup {
     @Test
     public void facebooklogin() {
         //giving platform name and url
-        SetupDriver("Windows", "https://www.facebook.com", "Chrome");
+        SetupDriver("Windows", "https://www.facebook.com", "Firefox");
 
         //Typing email
         driver.findElement(By.xpath("//*[@id='email']")).sendKeys("Type your Email");
@@ -60,7 +84,8 @@ public class Setup {
         driver.findElement(By.xpath("//*[@value='Log In']")).click();
 
 
-        //try and catching for exceptions
+        //giving thread.sleep to wait
+        //using try catch for bypassing exception
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
@@ -95,4 +120,107 @@ public class Setup {
         // driver.quit();
 
     }
+
+    @Test
+    public void helpValidated() {
+
+
+        //giving platform name and url
+        SetupDriver("Windows", "https://www.facebook.com", "Chrome");
+
+        //checking Messenger Link
+        driver.findElement(By.linkText("Help")).click();
+
+
+        //try and catching for exceptions
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            //don't want to see any print exception
+        }
+
+        //to exit browser
+        // driver.quit();
+
+    }
+
+    @Test
+    public void LanguageCheck() {
+
+
+        //giving platform name and url
+        SetupDriver("Windows", "https://www.facebook.com", "Chrome");
+
+        //checking Messenger Link
+        driver.findElement(By.linkText("Español")).click();
+
+
+        //try and catching for exceptions
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            //don't want to see any print exception
+        }
+
+        //to exit browser
+        // driver.quit();
+
+    }
+
+    @Test
+    public void ForgotaccCheck() {
+
+
+        //giving platform name and url
+        SetupDriver("Windows", "https://www.facebook.com", "Chrome");
+
+        //checking Messenger Link
+        driver.findElement(By.linkText("Forgot account?")).click();
+
+
+        //try and catching for exceptions
+        try {
+            Thread.sleep(6000);
+        } catch (InterruptedException e) {
+            //don't want to see any print exception
+        }
+
+        //to exit browser
+        // driver.quit();
+
+    }
+
+    @Test
+    public void CreatepageValidate() {
+
+
+        //giving platform name and url
+        SetupDriver("Windows", "https://www.facebook.com", "Firefox");
+
+        //checking Messenger Link
+        driver.findElement(By.linkText("Create a Page")).click();
+
+
+        //try and catching for exceptions
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            //don't want to see any print exception
+        }
+
+        //to exit browser
+        // driver.quit();
+
+    }
+
+
+
+
+
 }
+
+
+
+
+
+
